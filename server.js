d1cce965f745a0f1111a168a51b66473d34b516f
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-16 16:12:50 -0800

    Deployed to MongoDB atlas

--- server.js
+++ server.js
@@ -3,24 +3,35 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+// Default app PORT for use
 const PORT = 3000;
 
 const app = express();
 
+// Middleware declaration for routes
 app.use(logger("dev"));
-
+// Middleware declaration for compression
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
+// Parse incoming requests with urlencoded payloads and return as objects
+app.use(express.urlencoded({ extended: true }));
+// Return the incoming request objects as JSON objects
+app.use(express.json());
+// Serve static files in the public directory
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// Connecting to MongoDB via Mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+  }
+);
 
-// routes
+// Routes for data retrieval from DB
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
