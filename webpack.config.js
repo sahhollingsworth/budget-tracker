713763ab46a0edfdc050e820ab835f74c411b131
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-17 10:01:53 -0800

    Fix: incorrect image path in webpack.config.js
    Webpack translates __dirname as a / instead of a intuitive understanding of location

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ const config = {
             // Webpack isn't being used to generate html
             inject: false,
 
-            // Makes the names of the generated files predictable & easier to refer to them in app code
             fingerprints: false,
 
             name: "Budget Tracker App",
@@ -27,12 +26,12 @@ const config = {
             display: "standalone",
                 icons: [
                     {
-                    src: path.resolve( __dirname, "public/assets/images/icons/icon-512x512.png"),
+                    src: path.resolve( __dirname, "public/assets/icons/icon-512x512.png"),
                     // generate an image for each size in the size array
                     size: [72, 96, 128, 144, 152, 192, 384, 512]
                     },
                     {
-                    src: path.resolve( __dirname, "public/assets/images/icons/icon-192x192.png"),
+                    src: path.resolve( __dirname, "public/assets/icons/icon-192x192.png"),
                     size: [72, 96, 128, 144, 152, 192, 384, 512]
                     }
                 ]
